40f925f6d2636553bf1f4b961cedf97620c0f555
HANDSOMEXIET <214219@clvsc.tyc.edu.tw>
2024-12-05 13:50:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.title("Hello world")
+from datetime import date
+
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go 
+
+
+START = '2015-01-01'
+TODAY = date.today().strftime('%Y-%m-%d')
+
+st.title('Stock Prediction App')
+
+stocks=('AAPL','GOOGLE','MSFT','GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+n_years = st.slider('Years of prediction',1,4)
+period = n_years*365
